7060e21651793e828983aebdaffa95b8a9f32e56
subado <subado.main@gmail.com>
2023-12-15 20:38:45 +0400

    Add get_unit() and use Language

--- dicty/scraper/website.py
+++ dicty/scraper/website.py
@@ -1,14 +1,23 @@
+from dataclasses import dataclass
+
+from selenium.webdriver.remote.webdriver import BaseWebDriver
+
+from ..database.unit import Unit, Language
+
+
+@dataclass
 class Selectors:
-    def __init__(self, load_persuader: str, search_field: str):
-        self.load_persuader = load_persuader
-        self.search_field = search_field
+    load_persuader: str
+    search_field: str
 
 
 class Website:
-    def __init__(self, name: str, url: str, timeout: float, language_code: str, selectors: Selectors):
+    def __init__(self, name: str, url: str, timeout: float, language: Language, selectors: Selectors):
         self.name = name
         self.url = url
         self.timeout = timeout
-        # this code should be alpha_3 of one of pycountry.languages
-        self.language_code = language_code
+        self.language = language
         self.selectors = selectors
+
+    def get_unit(driver: BaseWebDriver) -> Unit:
+        pass
