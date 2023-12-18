4e6c937a27a38374240d875041ea7cdc8c37505a
subado <subado.main@gmail.com>
2023-12-18 22:13:44 +0400

    Add @handle_exception and fix types in clean_str

--- dicty/utils.py
+++ dicty/utils.py
@@ -1,8 +1,28 @@
+from typing import Callable, TypeVar, ParamSpec, Optional
+
+
 def clean_str(s: str, rubbish: str = '[],()'):
     for i in rubbish:
-        s = s.translate(str.maketrans({i: None}))
+        s = s.translate({ord(i): None})
     return s.strip()
 
 
 def normalize_str(s: str):
     return clean_str(s).lower()
+
+
+P = ParamSpec('P')
+R = TypeVar('R')
+E = TypeVar('E', bound=BaseException)
+
+
+def handle_exception(e: type[E], handler: Optional[Callable[[E], Optional[R]]] = None) -> Callable[
+        [Callable[P, R]], Callable[P, Optional[R]]]:
+    def wrapper(f: Callable[P, R]) -> Callable[P, Optional[R]]:
+        def inner(*args: P.args, **kwargs: P.kwargs) -> Optional[R]:
+            try:
+                return f(*args, **kwargs)
+            except e as exception:
+                return handler(exception) if handler is not None else None
+        return inner
+    return wrapper
