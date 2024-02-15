736cac8f4fca1845e483e14dddf3f12579390f4e
subado <subado.main@gmail.com>
2024-02-15 17:16:30 +0400

    Fix '/dictionaries' end-point

--- dicty/blueprints/dictionaries.py
+++ dicty/blueprints/dictionaries.py
@@ -8,15 +8,6 @@ from ..db.models import Dictionary
 bp = Blueprint('dictionaries', __name__, url_prefix='/dictionaries')
 
 
-@bp.route('/', methods=['GET'])
-def dictionaries(name: str):
-    dictionaries = db.session.execute(
-        db.select(Dictionary)
-        .order_by(Dictionary.name)
-    ).scalars()
-    return jsonify(dictionary)
-
-
 @bp.route('/<uuid:uuid>', methods=['GET'])
 def dictionary_by_id(uuid: UUID) -> str:
     dictionary = db.get_or_404(Dictionary, uuid)
@@ -30,3 +21,12 @@ def dictionary(name: str):
         .where(Dictionary.name == name)
     )
     return jsonify(dictionary)
+
+
+@bp.route('/', methods=['GET'])
+def dictionaries():
+    dictionaries = db.session.execute(
+        db.select(Dictionary)
+        .order_by(Dictionary.name)
+    ).scalars().all()
+    return jsonify(dictionaries)
