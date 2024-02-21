37dfd0ca03a51930d8a62870012e03e77aa152bc
subado <subado.main@gmail.com>
2024-02-21 21:29:06 +0400

    Add protos and script for generating Python output

---
+++ gen_proto.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+PROTO_PATH='proto/'
+PYTHON_OUT='dicty/proto'
+
+protos="$(find "$PROTO_PATH" -type f -name '*.proto')"
+
+python -m grpc_tools.protoc --proto_path="$PROTO_PATH" --python_out="$PYTHON_OUT" $protos &&
+	mv "$PYTHON_OUT/dicty"/* "$PYTHON_OUT" &&
+	rmdir "$PYTHON_OUT/dicty/"
